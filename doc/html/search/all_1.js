7bcf0bc90a123d10ecd0ccff434c098307959600
Ethan Ruffing <ruffinge@gmail.com>
2014-12-22 11:31:34 -0500

    Generated doxygen documentation

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['adc_5fuse_5fmutual_5fexclusion',['ADC_USE_MUTUAL_EXCLUSION',['../group__HAL__CONF.html#gac0893cb47e338c2dabad34b974a0a88d',1,'halconf.h']]],
+  ['adc_5fuse_5fwait',['ADC_USE_WAIT',['../group__HAL__CONF.html#ga39e892a4090185fbdda9bb105bc03b4f',1,'halconf.h']]]
+];
---
+++ doc/html/search/all_5.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['mac_5fuse_5fevents',['MAC_USE_EVENTS',['../group__HAL__CONF.html#ga887da1c1383a9e7b45c4464877a7e05e',1,'halconf.h']]],
+  ['mac_5fuse_5fzero_5fcopy',['MAC_USE_ZERO_COPY',['../group__HAL__CONF.html#gad763d9426413cf2fe3922ebb8578bc59',1,'halconf.h']]],
+  ['mmc_5fnice_5fwaiting',['MMC_NICE_WAITING',['../group__HAL__CONF.html#ga3087dfffa81dd8a0a80ee92746e65fe2',1,'halconf.h']]],
+  ['mmc_5fpolling_5fdelay',['MMC_POLLING_DELAY',['../group__HAL__CONF.html#ga6d164ed93ae233b2dc0c35bd19f75224',1,'halconf.h']]],
+  ['mmc_5fpolling_5finterval',['MMC_POLLING_INTERVAL',['../group__HAL__CONF.html#gac57e95210b57d6699c23bfb648e0904d',1,'halconf.h']]],
+  ['mmc_5fsector_5fsize',['MMC_SECTOR_SIZE',['../group__HAL__CONF.html#ga502a0143be4c303f3006c0888e1cec82',1,'halconf.h']]],
+  ['mmc_5fuse_5fspi_5fpolling',['MMC_USE_SPI_POLLING',['../group__HAL__CONF.html#ga463aaf38dda5f44ab579ddab0a1cf104',1,'halconf.h']]]
+];
