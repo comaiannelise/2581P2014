94fab91e51d4b279f00e605f15db98be30e7ef03
Ethan Ruffing <ruffinge@gmail.com>
2014-12-22 12:19:56 -0500

    Updated doxygen documentation

--- doc/html/search/pages_0.js
+++ doc/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['2581b_202014_20bcamsc_20robotics_20code',['2581P 2014 BCAMSC Robotics Code',['../md_README.html',1,'']]]
+  ['2581p_202014_20bcamsc_20robotics_20code',['2581P 2014 BCAMSC Robotics Code',['../md_README.html',1,'']]]
 ];
